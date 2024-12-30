dcadd987507ef49c34e505f3140065e0a8ef9f47
umar musa <umargafia@gmail.com>
2024-12-30 11:58:22 +0100

    change appbar name

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0">
             <Link to="/" className="text-xl font-bold text-blue-600">
-              CareClick
+              CareClik
             </Link>
           </div>
 
